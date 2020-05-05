438e31c444485dbfcef0eff9408d6dab73364cdf
Truong <truong.hoang@verztec.com>
2020-05-06 00:26:06 +0700

    profile

--- lib/views/dashboard/post/posts.dart
+++ lib/views/dashboard/post/posts.dart
@@ -4,11 +4,147 @@ import 'package:flutter/material.dart';
 class Posts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-        title: Text("Post")
-    ),
-    body: Container(),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            _headerView(
+              widthButton: width / 2,
+            ),
+            //_contentView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: camel_case_types, must_be_immutable
+class _headerView extends StatefulWidget {
+  final double widthButton;
+  int indexSelect = 0;
+
+  _headerView({this.widthButton});
+
+  @override
+  __headerViewState createState() => __headerViewState();
+}
+
+// ignore: camel_case_types
+class __headerViewState extends State<_headerView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 157,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 110,
+            color: Color(0x00ff34a3af),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  bottom: 20,
+                  left: 16,
+                  child: Text(
+                    "JobsRabbit",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              InkWell(
+                splashColor: Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    widget.indexSelect = 0;
+                  });
+                },
+                child: Container(
+                  height: 47,
+                  width: widget.widthButton,
+                  child: Stack(
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          "Posting",
+                          style: TextStyle(
+                            color: widget.indexSelect == 0
+                                ? Color(0xff4a4a4a)
+                                : Color(0xffa2a2a2),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                          color: widget.indexSelect == 0
+                              ? Color(0x00ff34a3af)
+                              : Color(0x00fff2f2f2),
+                          height: 3,
+                          width: widget.widthButton,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                splashColor: Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    widget.indexSelect = 1;
+                  });
+                },
+                child: Container(
+                  height: 47,
+                  width: widget.widthButton,
+                  child: Stack(
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          "Your Post",
+                          style: TextStyle(
+                            color: widget.indexSelect == 1
+                                ? Color(0xff4a4a4a)
+                                : Color(0xffa2a2a2),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                          color: widget.indexSelect == 1
+                              ? Color(0x00ff34a3af)
+                              : Color(0x00fff2f2f2),
+                          height: 3,
+                          width: widget.widthButton,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/dashboard/profile/profile.dart
+++ lib/views/dashboard/profile/profile.dart
@@ -5,10 +5,127 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: Text("Account")
-    ),
-    body: Container(),
+      body: Column(
+        children: <Widget>[_headerView()],
+      ),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class _headerView extends StatefulWidget {
+  @override
+  __headerViewState createState() => __headerViewState();
+}
+
+// ignore: camel_case_types
+class __headerViewState extends State<_headerView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 282,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0x00ff87d4dc),
+              Color(0x00ff34a3af),
+              Color(0x00ff34a3af)
+            ]),
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 62,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    print("tap");
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 40,
+                    width: 40,
+                    child: Image.asset(
+                      'assets/images/dashboard/profile/ic_setting.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                ),
+                Stack(
+                  children: <Widget>[
+                    SizedBox(height: 8,),
+                    Container(
+                      width: 86,
+                      height: 86,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(43.0)),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3
+                        )
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                        child: Image.network(
+                          "https://static.vinepair.com/wp-content/uploads/2017/03/darts-int.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: InkWell(
+                        onTap: () {
+                          print("tap");
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 25,
+                          width: 25,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(12.5)),
+                            color: Colors.white
+                          ),
+                          child: Image.asset(
+                            'assets/images/dashboard/profile/ic_edit_avatar.png',
+                            width: 14,
+                            height: 11,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                InkWell(
+                  onTap: () {
+                    print("tap");
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 40,
+                    width: 40,
+                    child: Image.asset(
+                      'assets/images/dashboard/profile/ic_noti.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
